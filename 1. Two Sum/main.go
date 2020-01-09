dae270681f392d389e775e583f042081f02494c4
sau00 <sau00gm@gmail.com>
2020-01-10 00:30:45 +0300

    day 1

---
+++ 1. Two Sum/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
